51c5bc062a8de8536e175290b2f63168bb45ce1d
Tom Gardiner <tgardiner@protonmail.com>
2024-11-17 14:46:17 -0600

    remove unecessary scryptSync logic

--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 
 An end-to-end example of:
 * Using diffie-hellman to generate a shared secret key.
-* Deriving an encryption/decryption key from the shared secret key.
-* Using that encryption key to encrypt a message with `aes-256-cbc`.
-* Decrypting that same message with the decryption key.
+* Using that shared key to encrypt a message with `aes-256-cbc`.
+* Decrypting that same message with the shared key.
 
 The code is written in a similar style to the official [Crypto](https://nodejs.org/api/crypto.html) docs, from the point of view of **Alice** & **Bob**.
 
--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const {
   createECDH,
   createCipheriv,
   createDecipheriv,
-  scryptSync,
   randomBytes
 } = require('node:crypto');
 
@@ -23,28 +22,22 @@ const bobPubKey = bob.generateKeys();
 const aliceSecret = alice.computeSecret(bobPubKey);
 const bobSecret = bob.computeSecret(alicePubKey);
 
-// Alice uses the secret key to generate an encryption key compatible with AES-256
-const aliceEncryptionKey = scryptSync(aliceSecret, '', 32);
-
 // Alice creates the message iv (do this for each new message)
 const aliceIv = randomBytes(16);
 
 // Alice encrypts the message
 const aliceMessage = Buffer.from('Hello World!');
-const cipher = createCipheriv('aes-256-cbc', aliceEncryptionKey, aliceIv);
+const cipher = createCipheriv('aes-256-cbc', aliceSecret, aliceIv);
 const encrypted = Buffer.concat([cipher.update(aliceMessage), cipher.final()]);
 
 // Alice prepends the iv to the message, and sends it to Bob
 encryptedMessage = Buffer.concat([aliceIv, encrypted]);
-
-// Bob uses the secret key to generate a decryption key compatible with AES-256
-const bobDecryptionKey = scryptSync(bobSecret, '', 32);
 
 // Bob splits the encrypted message into the iv and message
 messageIv = Buffer.copyBytesFrom(encryptedMessage, 0, 16);
 message = Buffer.copyBytesFrom(encryptedMessage, 16);
 
 // Bob decrypts the message
-const decipher = createDecipheriv('aes-256-cbc', bobDecryptionKey, messageIv);
+const decipher = createDecipheriv('aes-256-cbc', bobSecret, messageIv);
 const decrypted = Buffer.concat([decipher.update(message), decipher.final()]);
 console.log(`decrypted: ${decrypted.toString()}`);
